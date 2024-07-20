ef0aea5ba2ad4f252adfe97d7e8ba83d4ea85cb8
Ved Brahmbhatt <114875216+Ved1103@users.noreply.github.com>
2024-07-20 14:59:09 +0530

    Time: 4 ms (40.2%), Space: 7.9 MB (97.26%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -1,37 +1,70 @@
+// Approach 1 : T.C = O(N) S.C = O(N)
+// class Solution {
+// public:
+//     string removeDuplicateLetters(string s) {
+//         int n = s.size();
+//         unordered_map<char, int> mp;  
+//         unordered_set<char> seen;    
+
+//         stack<char> st;              
+
+//         for (char ch : s) {
+//             mp[ch]++;
+//         }
+
+//         for (int i = 0; i < n; ++i) {
+//             char temp = s[i];
+//             mp[temp]--;
+//             if (seen.find(temp) != seen.end()) {
+//                 continue;
+//             }
+            
+//             while (!st.empty() and st.top() > temp and mp[st.top()] > 0) {
+//                 char top = st.top();
+//                 st.pop();
+//                 seen.erase(top);
+//             }
+            
+//             st.push(temp);
+//             seen.insert(temp);
+//         }
+
+//         string result;
+//         while (!st.empty()) {
+//             result = st.top() + result;
+//             st.pop();
+//         }
+
+//         return result;
+//     }
+// };
+
+// APPROACH 2 T.C = O(N) S.C = O(1) AS HERE IM USING CONSTANT SPACE OF ARRAYS OF SIZE 26
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
         int n = s.size();
-        unordered_map<char, int> mp;  
-        unordered_set<char> seen;    
-
-        stack<char> st;              
-
+        int count[26] = {0};
+        bool visited[26] = {false};
+        string result = "";
         for (char ch : s) {
-            mp[ch]++;
+            count[ch - 'a']++;
         }
 
         for (int i = 0; i < n; ++i) {
-            char temp = s[i];
-            mp[temp]--;
-            if (seen.find(temp) != seen.end()) {
+            char ch = s[i];
+            count[ch - 'a']--;
+
+            if (visited[ch - 'a']) {
                 continue;
             }
-            
-            while (!st.empty() and st.top() > temp and mp[st.top()] > 0) {
-                char top = st.top();
-                st.pop();
-                seen.erase(top);
+            while (!result.empty() && result.back() > ch && count[result.back() - 'a'] > 0) {
+                visited[result.back() - 'a'] = false;
+                result.pop_back();
             }
-            
-            st.push(temp);
-            seen.insert(temp);
-        }
 
-        string result;
-        while (!st.empty()) {
-            result = st.top() + result;
-            st.pop();
+            result += ch;
+            visited[ch - 'a'] = true;
         }
 
         return result;
