92f234f997058529d456464bcb67f51dbb3a6728
Ved Brahmbhatt <114875216+Ved1103@users.noreply.github.com>
2024-08-05 19:03:34 +0530

    Time: 3 ms (51.62%), Space: 7.6 MB (37.76%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,29 +1,58 @@
+// class Solution {
+// public:
+//     bool checkValidString(string s) {
+//         stack<int> open, star; 
+//         for (int i = 0; i < s.size(); ++i) {
+//             if (s[i] == '(') {
+//                 open.push(i);
+//             } else if (s[i] == '*') {
+//                 star.push(i);
+//             } else {
+//                 if (!open.empty()) {
+//                     open.pop();
+//                 } else if (!star.empty()) {
+//                     star.pop();
+//                 } else {
+//                     return false;
+//                 }
+//             }
+//         }
+//         while (!open.empty() && !star.empty()) {
+//             if (open.top() > star.top()) {
+//                 return false;
+//             }
+//             open.pop();
+//             star.pop();
+//         }
+//         return open.empty();
+//     }
+// };
 class Solution {
 public:
     bool checkValidString(string s) {
-        stack<int> open, star; 
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == '(') {
-                open.push(i);
-            } else if (s[i] == '*') {
-                star.push(i);
+        int open = 0;
+        int close = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '(' || s[i] == '*') {
+                open++;
             } else {
-                if (!open.empty()) {
-                    open.pop();
-                } else if (!star.empty()) {
-                    star.pop();
-                } else {
-                    return false;
-                }
+                open--;
             }
-        }
-        while (!open.empty() && !star.empty()) {
-            if (open.top() > star.top()) {
+            if (open < 0) {
                 return false;
             }
-            open.pop();
-            star.pop();
         }
-        return open.empty();
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == ')' || s[i] == '*') {
+                close++;
+            } else {
+                close--;
+            }
+            if (close < 0) {
+                return false;
+            }
+        }
+        return true;
     }
 };
