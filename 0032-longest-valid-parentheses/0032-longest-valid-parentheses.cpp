4c7029aa7aea6e9034d7f7ab1cbf7d510178af02
Ved Brahmbhatt <114875216+Ved1103@users.noreply.github.com>
2024-08-07 19:16:10 +0530

    Time: 3 ms (73.92%), Space: 8.6 MB (85.02%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,25 +1,64 @@
+// class Solution {
+// public:
+//     int longestValidParentheses(string s) {
+//         stack<int> st;
+//         st.push(-1);
+//         int maxLen = 0;
+//         int n = s.size();
+
+//         for (int i = 0; i < n; i++) {
+//             char ch = s[i];
+//             if (ch == '(') {
+//                 st.push(i);
+//             } else {
+//                 st.pop();
+//                 if (st.empty()) {
+//                     st.push(i);
+//                 } else {
+//                     maxLen = max(maxLen, i - st.top());
+//                 }
+//             }
+//         }
+
+//         return maxLen;
+//     }
+// };
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        stack<int> st;
-        st.push(-1); 
-        int maxLen = 0; 
+        int open = 0, close = 0;
+        int maxLen = 0;
         int n = s.size();
-        
+
+        // Left to right traversal
         for (int i = 0; i < n; i++) {
-            char ch = s[i];
-            if (ch == '(') {
-                st.push(i);
-            } else {
-                st.pop();
-                if (st.empty()) {
-                    st.push(i); 
-                } else {
-                    maxLen = max(maxLen, i - st.top()); 
-                }
+            if (s[i] == '(') {
+                open++;
+            } else if (s[i] == ')') {
+                close++;
+            }
+            if (open == close) {
+                maxLen = max(maxLen, 2 * close);
+            } else if (close > open) {
+                open = close = 0;
             }
         }
-        
+
+        open = close = 0;
+        // Right to left traversal
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == '(') {
+                open++;
+            } else if (s[i] == ')') {
+                close++;
+            }
+            if (open == close) {
+                maxLen = max(maxLen, 2 * open);
+            } else if (open > close) {
+                open = close = 0;
+            }
+        }
+
         return maxLen;
     }
 };
