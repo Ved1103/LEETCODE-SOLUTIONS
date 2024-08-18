f631c4e2ba09ce155f4e3a0b1cdd590a9a5716e0
Ved Brahmbhatt <114875216+Ved1103@users.noreply.github.com>
2024-08-18 14:23:23 +0530

    Time: 40 ms (10.67%), Space: 32 MB (14.19%) - LeetHub

--- 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.cpp
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.cpp
@@ -9,3 +9,29 @@ public:
         return ans;
     }
 };
+// dp approach only if nums[i]>0
+// class Solution {
+// public:
+//     int minMoves(vector<int>& nums) {
+//         int n = nums.size();
+//         int minNum = *min_element(nums.begin(), nums.end());
+//         int dp[n][minNum + 1];
+        
+//         for (int j = 0; j <= minNum; j++) {
+//             dp[0][j] = abs(nums[0] - j);
+//         }
+
+//         for (int i = 1; i < n; i++) {
+//             for (int j = 0; j <= minNum; j++) {
+//                 dp[i][j] = dp[i-1][j] + abs(nums[i] - j);
+//             }
+//         }
+
+//         int ans = 1e9;
+//         for (int j = 0; j <= minNum; j++) {
+//             ans = min(ans, dp[n-1][j]);
+//         }
+
+//         return ans;
+//     }
+// };
