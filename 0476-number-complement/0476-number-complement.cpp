a18b94179e175427a6b39a5392902076c150412d
Ved Brahmbhatt <114875216+Ved1103@users.noreply.github.com>
2024-08-22 20:39:41 +0530

    Time: 3 ms (41.07%), Space: 7.7 MB (8.34%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int temp = num;
+        int count = 0;
+        while(num != 0){
+            num >>= 1;
+            count++;
+        }
+
+        int mask = pow(2, count) - 1;
+        return mask ^ temp;
+    }
+
+};
