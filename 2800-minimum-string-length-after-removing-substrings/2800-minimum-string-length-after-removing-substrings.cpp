6760694a37de372e5bce2f99d477d1143beb5fb5
Ved Brahmbhatt <114875216+Ved1103@users.noreply.github.com>
2024-10-07 22:25:23 +0530

    Time: 0 ms (100%), Space: 10 MB (93.92%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
     int minLength(string s) {
-        stack<char> st;
-        for (char ch : s) {
-            if (st.empty()) {
-                st.push(ch);
-                continue;
+        // stack<char> st;
+        // for (char ch : s) {
+        //     if (st.empty()) {
+        //         st.push(ch);
+        //         continue;
+        //     }
+        //     if (ch == 'B' and st.top() == 'A') {
+        //         st.pop();
+        //     } 
+        //     else if (ch == 'D' and st.top() == 'C') {
+        //         st.pop();
+        //     } 
+        //     else {
+        //         st.push(ch);
+        //     }
+        // }
+        // return st.size();
+
+        int n = s.size();
+        int i = 0;
+
+        for (int j = 0; j < n; ++j) {
+            s[i] = s[j];
+            if (i >= 1 && ((s[i] == 'B' && s[i-1] == 'A') || (s[i] == 'D' && s[i-1] == 'C'))) {
+                i -= 2;
             }
-            if (ch == 'B' and st.top() == 'A') {
-                st.pop();
-            } 
-            else if (ch == 'D' and st.top() == 'C') {
-                st.pop();
-            } 
-            else {
-                st.push(ch);
-            }
+            i++;
         }
-        return st.size();
+        
+        return i;
+
     }
 };
